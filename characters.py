089f43f45efe8ee507b25dccef34b23c546e809d
Ashley O <ashleyosbourne_3@hotmail.com>
2021-04-13 13:35:26 +0100

    Characters

---
+++ characters.py
@@ -0,0 +1,32 @@
+class Underpaid_NHS_Worker:
+backstory = "You run into an NHS worker who is under-paid and feeling unappreciated and overworked who the patronised by members of the public applauding them for their hard work and effort and you suddenly feel you are under and attack and, therefore, must engage in battle
+attack = 10
+def = 10
+health = 12
+
+
+class Karen:
+backstory = "You are approached by an aggressive woman. She is invading your personal space whilst attempting to enforce her own rules"
+attack = 15
+def = 8
+health = 20
+
+
+class Redundant_Robert:
+backstory = "You engage in conversation with man who has recently been made redundant. He is complaining about his current situation and your head is beginning to hurt"
+attack = 15
+def = 10
+health = 15
+
+
+class Pensioner_Pete:
+backstory = "You come across a worried and confusing elderly man. He has been in isolation for over a year with minimal human contract. He has become unsociable, cold and cruel. You are forced to defend yourself"
+attack = 10
+def = 5
+health = 7
+
+class Teenage_Tyrant:
+backstory = "You encounter an unruley teenager who is refusing to follow social distancing rules. They are coughing profusely and not wearing a face covering"
+attack = 15
+def = 15
+health = 10
